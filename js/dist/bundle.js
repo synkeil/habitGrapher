b59cc1a0aac5427c869089b269113ed6ddaa3eec
nicolas "sinkeil" b <NBOURGAREL@GMAIL.COM>
2016-10-18 21:13:42 +0200

    updating calendar visual/algoritme

--- js/dist/bundle.js
+++ js/dist/bundle.js
@@ -64,60 +64,112 @@
 
 	'use strict';
 
+	var Calendar = function calendarMainObject() {
+	  this.date = new Date();
+	  this.year = this.date.getFullYear();
+	  this.month = this.date.getMonth();
+	  this.day = this.date.getDate();
+	};
+
+	var cal = new Calendar();
 	var monthArr = ['Janvier', 'Fevrier', 'Mars', 'Avril', 'Mai', 'Juin', 'Juillet', 'Aout', 'Septembre', 'Octobre', 'Novembre', 'Décembre'];
 	var dayInMonthArray = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
-	var weekArr = ["Lundi", "Mardi", "Mercredi", "Jeudi", "Vendredi", "Samedi", "Dimanche"];
+	var weekArr = ['Lundi', 'Mardi', 'Mercredi', 'Jeudi', 'Vendredi', 'Samedi', 'Dimanche'];
+	var weekArrSrt = ['L', 'M', 'M', 'J', 'V', 'S', 'D'];
 	var hoursArr = [];
-	var date = new Date();
-	var year = date.getFullYear();
-	var month = date.getMonth();
-	var day = date.getDate();
-	var i = 0,
-	    j = 0,
-	    k = 0,
-	    l = 0;
+	var cycle = function cycle(elem, range) {
+	  if (elem > range) {
+	    return elem % range;
+	  }
+	  return elem;
+	};
 
-	for (i = 0; i < 24; i++) {
-	  hoursArr.push(i + "h:");
+	var i = 0;
+	var j = 0;
+	var k = 0;
+
+	for (i = 0; i < 24; i += 1) {
+	  hoursArr.push(i + 'h');
 	}
 
 	var leapCheck = function checkForLeapYear(check) {
 	  // Check for leap year and adjust feb days
 	  if (check === 1) {
-	    if (year % 4 === 0 && year % 100 !== 0 || year % 400 === 0) {
+	    if (cal.year % 4 === 0 && cal.year % 100 !== 0 || cal.year % 400 === 0) {
 	      dayInMonthArray[1] = 29;
 	    }
 	  }
 	};
 
-	var fancyCal = function initInFancyMode() {
-
+	var fancyView = function initInFancyMode() {
+	  // function to set the calendar in fancy mode
 	  monthArr.map(function (x) {
-
 	    leapCheck(j);
-
-	    iQ("#macroContent").append("<p>" + x + "</p><ul class='month" + j + "'></ul>");
-	    for (i = 0; i < dayInMonthArray[j]; i++) {
-	      date = new Date(year, j, i);
-	      iQ(".month" + j).append("<li>" + weekArr[date.getDay()] + " " + (i + 1) + "</li>");
+	    iQ('#macroContent').append('<p> ' + x + ' </p><ul class="month' + j + '" ></ul>');
+	    for (i = 0; i < dayInMonthArray[j]; i += 1) {
+	      cal.date = new Date(cal.year, j, i);
+	      iQ('.month' + j).append('<li>' + weekArr[cal.date.getDay()] + ' (' + (i + 1) + ')</li>');
 	    }
-	    j++;
+	    j += 1;
+	    return monthArr;
 	  });
 	};
 
-	var yearView = function initInYearView(check) {
+	var yearView = function initInYearView() {
+	  // function to set the calendar in year view
+	  var elMac = iQ('#macroContent').dom();
+	  var elMic = iQ('#microContent').dom();
+	  var newBoxMac = elMac.cloneNode(false);
+	  var newBoxMic = elMic.cloneNode(false);
 
-	  iQ("#macroContent").append(year);
+	  j = 0;
+	  k = 0;
+
+	  elMac.parentNode.replaceChild(newBoxMac, elMac);
+	  elMac = newBoxMac;
+	  elMic.parentNode.replaceChild(newBoxMic, elMic);
+	  elMic = newBoxMic;
+
+	  // append the scope (year/month/week/day)
+	  iQ('#macroContent').append(cal.year);
 
 	  monthArr.map(function (x) {
-
+	    // append the month to the main view
+	    // check for leap year
 	    leapCheck(j);
 
-	    iQ("#microContent").append("<div class='monthCell'>" + x + "</div>");
+	    cal.date = new Date(cal.year, j, 1);
+	    var strartingDigit = dayInMonthArray[(j + 11) % 12] - cal.date.getDay();
+	    var digit = 0;
 
-	    j++;
+	    console.log('in ' + cal.date + ' there are : ' + dayInMonthArray[(j + 12) % 12] + ' days and the first monday is a : ' + strartingDigit + ', and there are a total of ' + dayInMonthArray[(j + 11) % 12] + ' days in the previous month');
+	    // the actual appending (month cell/ days prep)
+	    iQ('#microContent').append('<div class="monthCell"><p>' + x + '</p><div class="dayLabels"></div><div class="dayDigits"></div></div>');
+
+	    weekArrSrt.map(function (y) {
+	      // append the weekdays shorthand
+	      iQ('.dayLabels').eq(j + 1).append('<div>' + y + '</div>');
+	      return weekArrSrt;
+	    });
+
+	    for (k = 0; k < 42; k += 1) {
+	      // append the day's digits
+	      digit = cycle(k + strartingDigit, dayInMonthArray[(j + 12) % 12]);
+
+	      cal.date = new Date(cal.year, cal.month, digit);
+	      iQ('.dayDigits').eq(j + 1).append('<div>' + digit + '</div>');
+	    }
+	    j += 1;
+	    return monthArr;
 	  });
 	};
+
+	iQ('#yearButton').listen('click', function () {
+	  yearView();
+	});
+	iQ('#fancyButton').listen('click', function () {
+	  fancyView();
+	});
 
 	yearView();
 
--- js/src/calendar.js
+++ js/src/calendar.js
@@ -1,65 +1,106 @@
-const monthArr = ['Janvier','Fevrier','Mars','Avril','Mai','Juin','Juillet','Aout','Septembre','Octobre','Novembre','Décembre'];
-const dayInMonthArray = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
-const weekArr = ["Lundi","Mardi","Mercredi","Jeudi","Vendredi","Samedi","Dimanche"];
-const hoursArr = [];
-let date = new Date();
-let year = date.getFullYear();
-let month = date.getMonth();
-let day = date.getDate();
-let i=0,j=0,k=0,l=0;
+const Calendar = function calendarMainObject() {
+  this.date = new Date();
+  this.year = this.date.getFullYear();
+  this.month = this.date.getMonth();
+  this.day = this.date.getDate();
+};
 
-for(i=0;i<24;i++){
-  hoursArr.push(i+"h:");
+const cal = new Calendar();
+const monthArr = ['Janvier', 'Fevrier', 'Mars', 'Avril', 'Mai', 'Juin', 'Juillet', 'Aout', 'Septembre', 'Octobre', 'Novembre', 'Décembre'];
+const dayInMonthArray = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
+const weekArr = ['Lundi', 'Mardi', 'Mercredi', 'Jeudi', 'Vendredi', 'Samedi', 'Dimanche'];
+const weekArrSrt = ['L', 'M', 'M', 'J', 'V', 'S', 'D'];
+const hoursArr = [];
+const cycle = (elem, range) => {
+  if (elem > range) {
+    return (elem % range);
+  }
+  return elem;
+};
+
+let i = 0;
+let j = 0;
+let k = 0;
+
+for (i = 0; i < 24; i += 1) {
+  hoursArr.push(`${i}h`);
 }
 
-const leapCheck = function checkForLeapYear(check){ // Check for leap year and adjust feb days
+const leapCheck = function checkForLeapYear(check) { // Check for leap year and adjust feb days
   if (check === 1) {
-    if ((year % 4 === 0 && year % 100 !== 0) || year % 400 === 0){
+    if ((cal.year % 4 === 0 && cal.year % 100 !== 0) || cal.year % 400 === 0) {
       dayInMonthArray[1] = 29;
     }
   }
-}
+};
 
 
-const fancyCal = function initInFancyMode(){
-
+const fancyView = function initInFancyMode() { // function to set the calendar in fancy mode
   monthArr.map(
-
-
-    (x)=>{
-
+    (x) => {
       leapCheck(j);
-
-      iQ("#macroContent").append("<p>"+x+"</p><ul class='month"+j+"'></ul>");
-      for(i=0;i<dayInMonthArray[j];i++){
-        date = new Date(year,j,i);
-        iQ(".month"+j).append("<li>"+weekArr[date.getDay()]+" "+(i+1)+"</li>");
+      iQ('#macroContent').append(`<p> ${x} </p><ul class="month${j}" ></ul>`);
+      for (i = 0; i < dayInMonthArray[j]; i += 1) {
+        cal.date = new Date(cal.year, j, i);
+        iQ(`.month${j}`).append(`<li>${weekArr[cal.date.getDay()]} (${i + 1})</li>`);
       }
-      j++;
+      j += 1;
+      return monthArr;
     }
-
   );
-}
+};
 
-const yearView = function initInYearView(check){
+const yearView = function initInYearView() { // function to set the calendar in year view
+  let elMac = iQ('#macroContent').dom();
+  let elMic = iQ('#microContent').dom();
+  const newBoxMac = elMac.cloneNode(false);
+  const newBoxMic = elMic.cloneNode(false);
 
-  iQ("#macroContent").append(year);
+  j = 0;
+  k = 0;
+
+  elMac.parentNode.replaceChild(newBoxMac, elMac);
+  elMac = newBoxMac;
+  elMic.parentNode.replaceChild(newBoxMic, elMic);
+  elMic = newBoxMic;
+
+  // append the scope (year/month/week/day)
+  iQ('#macroContent').append(cal.year);
 
   monthArr.map(
-
-
-    (x)=>{
-
+    (x) => { // append the month to the main view
+      // check for leap year
       leapCheck(j);
 
-      iQ("#microContent").append("<div class='monthCell'>"+x+"</div>");
+      cal.date = new Date(cal.year, j, 1);
+      const strartingDigit = dayInMonthArray[((j + 11) % 12)] - (cal.date.getDay());
+      let digit = 0;
 
-      j++;
+      console.log(`in ${cal.date} there are : ${dayInMonthArray[((j + 12) % 12)]} days and the first monday is a : ${strartingDigit}, and there are a total of ${dayInMonthArray[((j + 11) % 12)]} days in the previous month`);
+      // the actual appending (month cell/ days prep)
+      iQ('#microContent').append(`<div class="monthCell"><p>${x}</p><div class="dayLabels"></div><div class="dayDigits"></div></div>`);
 
+      weekArrSrt.map(
+        (y) => { // append the weekdays shorthand
+          iQ('.dayLabels').eq(j + 1).append(`<div>${y}</div>`);
+          return weekArrSrt;
+        }
+      );
+
+      for (k = 0; k < 42; k += 1) { // append the day's digits
+        digit = cycle(((k) + strartingDigit), dayInMonthArray[((j + 12) % 12)]);
+
+        cal.date = new Date(cal.year, cal.month, digit);
+        iQ('.dayDigits').eq(j + 1).append(`<div>${digit}</div>`);
+
+      }
+      j += 1;
+      return monthArr;
     }
-
   );
+};
 
-}
+iQ('#yearButton').listen('click', () => { yearView(); });
+iQ('#fancyButton').listen('click', () => { fancyView(); });
 
 yearView();
